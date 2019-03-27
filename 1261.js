76081538c56efe1020c1fdf553703124fcf3bef0
이름 <46488489+name0825@users.noreply.github.com>
2019-03-27 16:30:52 +0900

    Update 1261.js

--- 1261.js
+++ 1261.js
@@ -1,7 +1,8 @@
+/* http://codeup.kr/problem.php?id=1261*/
 function noname(arr) {
  var box=[];
  for (i in arr) {
   if (arr[i]%5==0) box.push(arr[i]);
  }
- return box.length?box?0;
+ return box.length?box:0;
 }
